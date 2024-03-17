cfbe4833e8548d3da9c58f2783d60a4857e1104e
Surya <118984729+suryachan1@users.noreply.github.com>
2024-03-17 07:01:17 +0530

    Delete Patterns directory

--- Patterns/ex1.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,4):
-    for j in range(1,6):
-        print('x',end=' ')
-    print()
--- Patterns/ex9.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,5):
-    for s in range(1,4-i+1):
-        print(' ',end=' ')
-    for j in range(1,2*i):
-        print('*',end=' ')
-    print()
